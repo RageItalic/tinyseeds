7921581050425b1f068c2c1e4bc5219088c56c49
pauldumitriu <paul.dumitriu01@gmail.com>
2022-11-30 00:24:11 -0500

    finished checkout feature from backend. Poop

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,69 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require("cors")
-const PORT = 8080
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51M9GvcJYmNxO345SdKOJFgFm1WvIV7A8rwzRTrSnQCsvfrG44YDS9kdwGqAwQopjPqRGeDzkxak8JQ4VnQyPOFT900k95HAA75"
+);
+const PORT = 8080;
 
 //this allows us to access JSON data sent from frontend to the backend
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //this allows the frontend to talk to the backend
-app.use(cors())
+app.use(cors());
+
+//Stripe
+
+// app.use(express.static("public"));
+
+app.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: "price_1M9hnaJYmNxO345SLhLn5Lto",
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `http://localhost:8080/success`, //NEEDS TO BE UPDATED
+    cancel_url: `http://localhost:8080/error`,
+  });
+
+  console.log(session.url);
+  res.status(200).json({ url: session.url });
+});
+
+app.get("/success", (req, res) => {
+  console.log("Stripe succeeded, Poop\n");
+});
+app.get("/error", (req, res) => {
+  console.log("Stripe failed, Poop\n");
+});
 
 app.get("/", (request, response) => {
-    console.log("pinged: ", request.url, " the path is: ", request.path)
-    console.log("params if any: ", request.params)
-    response.status(200).json({
-        message: "Backend working! Message to front end being sent!"
-    })
-})
+  console.log("pinged: ", request.url, " the path is: ", request.path);
+  console.log("params if any: ", request.params);
+  response.status(200).json({
+    message: "Backend working! Message to front end being sent!",
+  });
+});
 
 app.post("/sendData", (request, response) => {
-    console.log("Data should be in ", request.body)
-    //accessing data example
-    let dataHolder = request.body
+  console.log("Data should be in ", request.body);
+  //accessing data example
+  let dataHolder = request.body;
 
-    //manipulating data example (if necessary)
-    dataHolder["received"] = true
-    dataHolder["message"] = "Data received and manipulated and sent back."
-    dataHolder["favFoods"].push("Thai Green Curry")
+  //manipulating data example (if necessary)
+  dataHolder["received"] = true;
+  dataHolder["message"] = "Data received and manipulated and sent back.";
+  dataHolder["favFoods"].push("Thai Green Curry");
 
-    //sending data back to frontend example
-    response.status(200).json(dataHolder)
-})
-
+  //sending data back to frontend example
+  response.status(200).json(dataHolder);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server started. Listening on http://localhost:${PORT}`)
-})
+  console.log(`Server started. Listening on http://localhost:${PORT}`);
+});
--- frontend/src/utils/firebase.js
+++ frontend/src/utils/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
-import {getDatabase} from 'firebase/database'
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,7 +16,7 @@ const firebaseConfig = {
   messagingSenderId: "255105946857",
   appId: "1:255105946857:web:8482211f70f97735becc78",
   measurementId: "G-MNQ600CHJ7",
-  databaseURL: "https://tinyseeds-2bf49-default-rtdb.firebaseio.com/"
+  databaseURL: "https://tinyseeds-2bf49-default-rtdb.firebaseio.com/",
 };
 
 // Initialize Firebase
