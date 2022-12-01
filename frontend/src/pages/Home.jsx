f67fbd9a786e56b6e42078a3f327466232be3105
itsnoor24 <itsnoorabbas@gmail.com>
2022-12-01 13:09:59 -0500

    updated homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,31 +1,28 @@
-import "../styles/home.css";
-import img from "../assets/homeImage.png";
+import homeStyles from "../styles/home.module.css";
+import img from "../assets/homePic.png";
 
 const Home = () => {
   return (
-    <div className="home">
+    <div className={homeStyles.home}>
       {/* <img src={img} alt="plant pic" id="myImage" /> */}
-      <div className="image">
-        <img src={img} alt="plant pic" id="myImage" />
-      </div>
-      <div className="title">
-        <h1>
-          <em>tiny</em>
-          <br></br>seeds
-        </h1>
-      </div>
-      <div className="description">
-        <p>
-          <b>We love plants like you do</b>
-          <br></br>
-          tiny seeds is a modern <br></br> plants e-commerce store.
-        </p>
-      </div>
-
-      <div className="button">
-        <button type="button" id="myButton">
-          Go Shopping
-        </button>
+      <div className={homeStyles.flexContainer}>
+        <div className={homeStyles.image} id={homeStyles.flexItem1}>
+          <img src={img} alt="plant pic" id={homeStyles.myImage} />
+        </div>
+        <div className={homeStyles.title} id={homeStyles.flexItem2}>
+          <h1>
+            <em>tiny</em>
+            <br></br>seeds
+            <p className={homeStyles.description}>
+              <br></br>
+              <b>We love plants like you do</b>
+              <p>tiny seeds is a modern plants e-commerce store.</p>
+            </p>
+            <button type="button" id={homeStyles.myButton}>
+              Go Shopping
+            </button>
+          </h1>
+        </div>
       </div>
     </div>
   );
