0d885e7679fb80a049a3d5967aba0eb0b13d855e
itsnoor24 <itsnoorabbas@gmail.com>
2022-11-30 19:19:29 -0500

    updated homePage style

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,34 @@
-import '../styles/home.css'
+import "../styles/home.css";
+import img from "../assets/homeImage.png";
 
 const Home = () => {
-    return (
-        <div className="container">
-            <h1>Home page</h1>
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      {/* <img src={img} alt="plant pic" id="myImage" /> */}
+      <div className="image">
+        <img src={img} alt="plant pic" id="myImage" />
+      </div>
+      <div className="title">
+        <h1>
+          <em>tiny</em>
+          <br></br>seeds
+        </h1>
+      </div>
+      <div className="description">
+        <p>
+          <b>We love plants like you do</b>
+          <br></br>
+          tiny seeds is a modern <br></br> plants e-commerce store.
+        </p>
+      </div>
 
-export default Home
+      <div className="button">
+        <button type="button" id="myButton">
+          Go Shopping
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
