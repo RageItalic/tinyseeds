3bff0251c478eb126bf571b03cc886a8f2bb492e
pauldumitriu <paul.dumitriu01@gmail.com>
2022-12-05 13:30:32 -0500

    Imporved performance

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -9,28 +9,31 @@ import {
 
 export async function getOrderHistory(userId) {
   const db = getDatabase();
-  var resu = [];
+  var results = [];
   try {
     const snapshot = await get(ref(db, `/purchaseOrders`));
     if (snapshot.exists()) {
-      snapshot.forEach(function (childSnapshot) {
-        //we are in the child node (purchaseOrder[n]) we need to go into the object to check fields
-        childSnapshot.forEach(function (grandChildSnapshot) {
-          //we are looping through all the keys in each object, hence we check for the buyerID field and make sure its equal to userId provided
-          if (
-            grandChildSnapshot.key == "buyerID" &&
-            grandChildSnapshot.val() == userId
-          ) {
-            var json = childSnapshot.toJSON();
-            resu.push(json);
-          }
-        });
-      });
+      //   snapshot.forEach(function (childSnapshot) {
+      //     //we are in the child node (purchaseOrder[n]) we need to go into the object to check fields
+      //     childSnapshot.forEach(function (grandChildSnapshot) {
+      //       //we are looping through all the keys in each object, hence we check for the buyerID field and make sure its equal to userId provided
+      //       if (
+      //         grandChildSnapshot.key == "buyerID" &&
+      //         grandChildSnapshot.val() == userId
+      //       ) {
+      //         var json = childSnapshot.toJSON();
+      //         results.push(json);
+      //       }
+      //     });
+      //   });
+
+      results = snapshot.val().filter((node) => node.buyerID === userId);
     } else {
       console.log("no data found");
     }
   } catch (e) {
     console.error(e);
   }
-  console.log(resu);
+  console.log(results);
+  return results;
 }
