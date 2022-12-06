1cc2160b865df67e5ec346ac0f5b54a38b487107
pauldumitriu <paul.dumitriu01@gmail.com>
2022-12-05 19:46:33 -0500

    Updated function to return object which contains plant and qty in order.

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,39 +1,45 @@
 import axios from "axios";
-import {
-  getDatabase,
-  get,
-  ref,
-  orderByChild,
-  equalTo,
-} from "firebase/database";
+import { getDatabase, get, ref } from "firebase/database";
 
 export async function getOrderHistory(userId) {
   const db = getDatabase();
-  var results = [];
+  var orders = [];
   try {
     const snapshot = await get(ref(db, `/purchaseOrders`));
     if (snapshot.exists()) {
-      //   snapshot.forEach(function (childSnapshot) {
-      //     //we are in the child node (purchaseOrder[n]) we need to go into the object to check fields
-      //     childSnapshot.forEach(function (grandChildSnapshot) {
-      //       //we are looping through all the keys in each object, hence we check for the buyerID field and make sure its equal to userId provided
-      //       if (
-      //         grandChildSnapshot.key == "buyerID" &&
-      //         grandChildSnapshot.val() == userId
-      //       ) {
-      //         var json = childSnapshot.toJSON();
-      //         results.push(json);
-      //       }
-      //     });
-      //   });
-
-      results = snapshot.val().filter((node) => node.buyerID === userId);
+      orders = snapshot.val().filter((node) => node.buyerID === userId);
     } else {
       console.log("no data found");
     }
   } catch (e) {
     console.error(e);
   }
-  console.log(results);
-  return results;
+
+  //compile all plants ordered by user into an array
+  var plantsOrdered = [];
+  orders.forEach((element) => {
+    element.productsBought.forEach((plant) => {
+      plantsOrdered.push(plant);
+    });
+  });
+
+  var resu = [];
+  plantsOrdered.forEach(async (plant) => {
+    try {
+      const snapshot = await get(ref(db, `/plants`));
+      if (snapshot.exists()) {
+        snapshot.val().filter((node) => {
+          if (node.id === plant.productId) {
+            resu.push(node);
+            node["qty"] = plant.qty;
+            resu.push(node);
+          }
+        });
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  });
+  console.log(resu);
+  return orders;
 }
