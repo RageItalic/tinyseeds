c9f3cd0824d1149c943796779962de9e5ba72c37
Claudia Farkas <claudialillafarkas@gmail.com>
2022-12-10 12:55:57 -0500

    Added error page Ui

--- frontend/src/pages/CheckoutError.jsx
+++ frontend/src/pages/CheckoutError.jsx
@@ -2,13 +2,22 @@ import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { useAuth } from "../hooks/useAuth"
 import useAuthStore from "../store/auth"
-
+import styles from "../styles/checkoutError.module.css";
 
 const CheckoutError = () => {
     return (
-        <div>
-            <h1>Checkout Failed!</h1>
-            <p>Your order has not been placed. Try again.</p>
+        <div className={styles.container}>
+            <div className={styles.CheckoutWrapper}>
+                <div className={styles.h1_container}>
+                    <h1>Checkout Failed!</h1>
+                </div>
+                <div className={styles.pic_container}>
+                    <img src="/src/assets/error.png" alt="checkout" />
+                </div>
+                <div className={styles.p_container}>
+                    <p>Your order has not been placed. Try again.</p>
+                </div>
+            </div>
         </div>
     )
 }
