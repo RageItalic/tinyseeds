98c07d4db252bc198b27bc8189784663091f9451
itsnoor24 <itsnoorabbas@gmail.com>
2022-12-14 22:26:57 -0500

    Created draft of orders page

--- frontend/src/pages/PastOrders.jsx
+++ frontend/src/pages/PastOrders.jsx
@@ -1,7 +1,69 @@
+import orderStyles from "../styles/pastOrders.module.css";
+import useAuthStore from "../store/auth";
+import { auth } from "../utils/firebase";
+
 const PastOrders = () => {
+  const user = useAuthStore((state) => state.user);
   return (
     <div>
-      <p>defrgthy</p>
+      <div>
+        <h1 id={orderStyles.title}>Orders</h1>
+      </div>
+      <div>
+        <table className={orderStyles.myTable}>
+          <tr id={orderStyles.row}>
+            <th id={orderStyles.header}> User</th>
+            <th id={orderStyles.header}> Products</th>
+            <th id={orderStyles.header}> Date of Purchase</th>
+            <th id={orderStyles.header}> Status of Order</th>
+          </tr>
+          <tr>
+            <td id={orderStyles.infoUnique}>
+              {user.displayName ?? user.email}
+            </td>
+            <td id={orderStyles.infoUnique}>test</td>
+            <td id={orderStyles.infoUnique}>test</td>
+            <td id={orderStyles.infoUnique}>test</td>
+          </tr>
+          <tr>
+            <td id={orderStyles.info}>{user.displayName ?? user.email}</td>
+            <td id={orderStyles.info}>test</td>
+            <td id={orderStyles.info}>test</td>
+            <td id={orderStyles.info}>test</td>
+          </tr>
+          <tr>
+            <td id={orderStyles.infoUnique}>
+              {user.displayName ?? user.email}
+            </td>
+            <td id={orderStyles.infoUnique}>test</td>
+            <td id={orderStyles.infoUnique}>test</td>
+            <td id={orderStyles.infoUnique}>test</td>
+          </tr>
+          <tr>
+            <td id={orderStyles.info}>{user.displayName ?? user.email}</td>
+            <td id={orderStyles.info}>test</td>
+            <td id={orderStyles.info}>test</td>
+            <td id={orderStyles.info}>test</td>
+          </tr>
+          <tr>
+            <td id={orderStyles.infoUnique}>
+              {user.displayName ?? user.email}
+            </td>
+            <td id={orderStyles.infoUnique}>test</td>
+            <td id={orderStyles.infoUnique}>test</td>
+            <td id={orderStyles.infoUnique}>test</td>
+          </tr>
+        </table>
+        <br></br>
+        <div className={orderStyles.buttons}>
+          <a href="#" className={orderStyles.previous} id={orderStyles.round}>
+            &#8249;
+          </a>
+          <a href="#" className={orderStyles.next} id={orderStyles.round}>
+            &#8250;
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
